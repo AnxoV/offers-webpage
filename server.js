f6959ff80dd80e90b7ebb647e7cfed6b1bcc20ce
AnxoV <anxovilasm@gmail.com>
2024-09-10 12:13:56 +0000

    feat(nodejs): implement routes and views

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3500;
+
+
+/**
+ * Middleware
+ */
+
+// Static files
+app.use("/", express.static(path.join(__dirname, "public")));
+// Routes
+app.use("/", require("./routes/root"));
+
+// Page not found
+app.all("*", function(request, response) {
+    response.status(404);
+    if (request.accepts("html")) {
+        response.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (request.accepts("json")) {
+        response.json({
+            "error": "404 Not Found"
+        });
+    } else {
+        response.type("txt");
+        response.send("404 Not Found");
+    }
+});
+
+
+app.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}`);
+});
