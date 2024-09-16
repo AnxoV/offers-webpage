db33186f94546ffb9d051da08ff419addeeb2046
AnxoV <anxovilasm@gmail.com>
2024-09-16 10:30:25 +0000

    feat(html): add register page for students and companies

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 const registerController = require("../controllers/registerController");
+
+router.get("/student(.html)?", function(request, response) {
+    response.sendFile(path.join(__dirname, "..", "views", "register", "student.html"));
+});
+
+router.get("/company(.html)?", function(request, response) {
+    response.sendFile(path.join(__dirname, "..", "views", "register", "company.html"));
+});
 
 router.post("/student", registerController.handleNewStudent);
 router.post("/company", registerController.handleNewCompany);
